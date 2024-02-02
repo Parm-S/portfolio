8d271d95153277f4f4f19aaf5cbb990d9e525a60
parminder-futurionic <parminder.futurionic@gmail.com>
2024-02-03 03:24:03 +0530

    update project card

--- components/Project/ProjectCard.tsx
+++ components/Project/ProjectCard.tsx
@@ -35,7 +35,8 @@ const ProjectCard = ({
     >
       <section
         className=" bg-gray-100 max-w-[42rem] 
-                 border border-black/5 rounded-lg overflow-hidden 
+                 border border-black/5 rounded-lg 
+                 overflow-hidden 
                   sm:pr-8 sm:h-[20rem]  relative
                 hover:bg-gray-200 transition
                 dark:bg-white/10 dark:hover:bg-white/20
@@ -45,8 +46,8 @@ const ProjectCard = ({
         <div
           className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 
           sm:pt-10 sm:max-w-[50%]
-     flex flex-col h-full even:pl-12 
-     group-even:ml-[21rem]
+     flex flex-col h-full sm:group-even:pl-8 
+     sm:group-even:ml-[21rem]
      "
         >
           <h3 className="text-2xl font-semibold ">{title}</h3>
