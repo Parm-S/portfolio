d02f34afd36e220f9b0261662fb6b0c1f2df1a53
parminder-futurionic <parminder.futurionic@gmail.com>
2024-02-02 20:36:08 +0530

    added Experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import About from "@/components/About";
+import Experience from "@/components/Experience";
 import Intro from "@/components/Intro";
 import Project from "@/components/Project";
 import SectionDivider from "@/components/SectionDivider";
@@ -12,6 +13,7 @@ export default function Home() {
       <About />
       <Project />
       <Skills />
+      <Experience />
     </main>
   );
 }
